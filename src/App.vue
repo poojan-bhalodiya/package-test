09b90bc112ed7a22eaec62d8b3f9df100e8bf5f1
legalauction <legalauction4@gmail.com>
2025-05-02 22:50:44 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import { useLocalStorage, useSessionStorage } from "@imtapan/re-storage";
+
+const local = useLocalStorage("myKey", "defaultValue");
+local.set("Hello World");
+console.log(local.get()); // 'newValue'
+
+const session = useSessionStorage("sessionKey", { loggedIn: false });
+session.set({ loggedIn: true });
+console.log(session.get()); // { loggedIn: true }
+</script>
+
+<template>
+  <p>Hello World</p>
+</template>
+
+<style scoped></style>
